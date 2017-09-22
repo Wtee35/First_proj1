446ae188bccd516e2e99bf90119436556ad90328
Wtee35 <wtee@rambler.ru>
2017-09-22 22:24:34 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+var http = require("http");
+http.createServer(function(request,response){
+     
+    response.end(greeting.getMessage(userName) +"." + " Let me introduce my first project");
+    
+     
+}).listen(5555, "127.0.0.1",function(){
+    console.log("Сервер доступен на порту 5555");
+});
+
+var os = require("os");
+// получим имя текущего пользователя
+var userName = os.userInfo().username;
+ 
+console.log("Имя пользователя :" + userName);
+
+var greeting = require("./greeting");
+
+
+console.log(`Дата запроса: ${greeting.date}`);
+console.log(greeting.getMessage(userName));
