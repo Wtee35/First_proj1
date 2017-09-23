8e085e1617b70a23df81f5610512db6e10d237f0
Wtee35 <32204250+Wtee35@users.noreply.github.com>
2017-09-23 14:54:20 +0300

    Update app.js
    Some changes

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 var http = require("http");
 http.createServer(function(request,response){
-     
-    response.end(greeting.getMessage(userName) +"." + " Let me introduce my first project !");
+     response.end(greeting.getMessage(userName) +"." + " Let me introduce my first project !");
     
-     
 }).listen(5555, "127.0.0.1",function(){
     console.log("Сервер доступен на порту 5555");
 });
 
 var os = require("os");
-// получим имя текущего пользователя
+
 var userName = os.userInfo().username;
  
 console.log("Имя пользователя :" + userName);
@@ -20,7 +18,6 @@ var User = require("./user.js");
  
 var andrew = new User("Andrew", 24);
 andrew.sayHi();
-
 
 console.log(`Дата запроса: ${greeting.date}`);
 console.log(greeting.getMessage(userName));
